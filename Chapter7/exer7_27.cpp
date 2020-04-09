599a053304d76b976826b920d5efa79c95b0046e
锦罽暖亲猫 <55181876+bhan-zju@users.noreply.github.com>
2020-04-09 11:25:44 +0800

    Add files via upload

---
+++ Chapter7/exer7_27.cpp
@@ -0,0 +1,18 @@
+﻿// exer7_27.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Screen.h"
+
+int main()
+{
+    Screen myScreen(5, 5, 'X');
+    myScreen.move(4, 0).set('#').display(std::cout);
+    std::cout << "\n";
+    myScreen.display(std::cout);
+    std::cout << "\n";
+
+    return 0;
+}
+
+// 
---
+++ Chapter7/prog7_3_2.cpp
@@ -0,0 +1,24 @@
+﻿// prog7_3_2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include "Screen.h"
+
+int main()
+{
+    Screen myScreen(5, 3);
+    myScreen.move(4, 0).set('#');    // 等价于   myScreen.move(4, 0);   myScreen.set('#'); 
+
+    myScreen.display(std::cout);         // 将调用 Screen& display(std::ostream& os) 
+    std::cout << std::endl;
+    myScreen.set('*').display(std::cout);     // 将调用 Screen& display(std::ostream& os) 
+    std::cout << std::endl;
+
+    const Screen blank(5, 3);
+    blank.display(std::cout);         // 将调用 const Screen& display(std::ostream& os) const
+    std::cout << std::endl;
+
+    return 0;
+}
+
+// 
