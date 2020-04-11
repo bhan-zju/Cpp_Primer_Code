71d4752686663fa389e6ee0ce6aebcd037fab973
锦罽暖亲猫 <55181876+bhan-zju@users.noreply.github.com>
2020-04-11 14:25:56 +0800

    Add files via upload

---
+++ Chapter7/prog7_4_1.cpp
@@ -0,0 +1,51 @@
+﻿// prog7_4_1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+typedef double Money;
+std::string bal;
+class Account {
+public:
+    Money balance() { return bal; }      // 返回的是类内的Money类型的bal
+private:
+    //typedef double Money;          // 错误：类型名字不能重新定义
+    Money bal;
+};
+
+int height;
+class Screen {
+public:
+    typedef std::string::size_type pos;
+    void dummy_fcn(pos height) {
+        cursor = width * height;     // 这里的height是该函数的输入参数，覆盖掉了类内的height
+    }
+    void dummy_fcn2(pos height) {
+        cursor = width * this->height;        // 如果想用到类内的height
+        // 或
+        cursor = width * Screen::height;
+    }
+    void dummy_fcn3(pos height) {
+        cursor = width * ::height;            // 如果想用到类外层作用域中的height
+    }
+    void setHeight(pos);
+
+private:
+    pos cursor;
+    pos height = 0, width = 0;
+};
+Screen::pos verify(Screen::pos);       // 该函数位于 Screen类定义 之后，类内setHeight函数定义 之前，可以被 setHeight函数 调用
+void Screen::setHeight(pos var) {
+    // var: 参数
+    // height: 类的成员
+    // verify: 全局函数
+    height = verify(var);
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+//
---
+++ Chapter7/prog7_5.cpp
@@ -0,0 +1,40 @@
+﻿// prog7_5.cpp : 构造函数初始值列表
+//
+
+#include <iostream>
+
+class ConstRef {
+public:
+    ConstRef(int ii);    // 构造函数
+private:
+    int i;
+    const int ci;
+    int& ri;
+};
+// 错误：ci和ri必须被初始化,而不能被赋值
+/*
+ConstRef::ConstRef(int ii) {
+    i = ii;
+    ci = ii;      // 错误：不能给const赋值
+    ri = i;       // 错误：ri没有被初始化
+}
+*/
+// 正确的写法：显式地初始化 引用 和 const成员
+ConstRef::ConstRef(int ii): i(ii), ci(ii), ri(i) { }
+
+// 成员初始化的顺序：与它们在类定义中出现的顺序一致
+class X {
+    int i;
+    int j;
+public:
+    // 构造函数
+    //X(int val):j(val), i(j) { }       // i 比 j 先初始化，因此 i 将是未定义的
+    X(int val) :j(val), i(val) { }      // 正确写法，或 X(int val):i(val), j(i) { }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// 
