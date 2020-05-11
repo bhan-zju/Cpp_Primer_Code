623d7c92116723cbdf3bdd77eb65418bf2f48fac
锦罽暖亲猫 <55181876+bhan-zju@users.noreply.github.com>
2020-05-11 10:11:27 +0800

    Add files via upload

---
+++ Chapter8/prog8_3_1.cpp
@@ -0,0 +1,38 @@
+﻿// prog8_3_1.cpp : istringstream
+//
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+struct PersonInfo
+{
+    std::string name;
+    std::vector<std::string> phones;
+};
+
+int main()
+{
+    std::string line, word;
+    std::vector<PersonInfo> people;
+
+    while (getline(std::cin, line)) {            // 从标准输入读入数据
+        PersonInfo info;
+        std::istringstream record(line);     // 将record绑定到字符串line
+        record >> info.name;           // 从line中读取名字
+        while (record >> word) {
+            info.phones.push_back(word);
+        }
+        people.push_back(info);
+    }
+
+    // 检验程序
+    for (auto& member : people) {
+        std::cout << member.name << ":" << std::endl;
+        for (auto& phone : member.phones) {
+            std::cout << phone << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
