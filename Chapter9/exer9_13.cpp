5aca8150ef78df8a92e55993800910fc57acd4c0
锦罽暖亲猫 <55181876+bhan-zju@users.noreply.github.com>
2020-05-18 10:43:10 +0800

    Add files via upload

---
+++ Chapter9/exer9_13.cpp
@@ -0,0 +1,24 @@
+﻿// exer9_13.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+int main()
+{
+    std::list<int> ilis{ 8,9,7,1,3,4,6,0 };
+    std::vector<double> dvec(ilis.begin(), ilis.end());
+    for (auto d : dvec) {
+        std::cout << d << " ";
+    }
+    std::cout << std::endl;
+
+    std::vector<int> ivec{ 8,9,7,1,3,4,6,0 };
+    std::vector<double> dvec2(ivec.begin(), ivec.end());
+    for (auto d : dvec2) {
+        std::cout << d << " ";
+    }
+    std::cout << std::endl;
+}
+
