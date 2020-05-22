14b2e17c6e8bfdbe5ec504ad3f737f188106746a
锦罽暖亲猫 <55181876+bhan-zju@users.noreply.github.com>
2020-05-22 15:44:56 +0800

    Add files via upload

---
+++ Chapter9/exer9_18.cpp
@@ -0,0 +1,20 @@
+﻿// exer9_18.cpp : 
+//
+
+#include <iostream>
+#include <deque>
+
+int main()
+{
+    std::string word;
+    std::deque<std::string> sde;
+    while (std::cin >> word)
+        sde.push_back(word);
+
+    for (auto iter = sde.begin(); iter != sde.end(); ++iter)
+        std::cout << *iter << std::endl;
+
+    return 0;
+}
+
+// 
---
+++ Chapter9/exer9_20.cpp
@@ -0,0 +1,31 @@
+﻿// exer9_20.cpp : 
+//
+
+#include <iostream>
+#include <list>
+#include <deque>
+
+int main()
+{
+    std::list<int> ilist{ 8,9,7,1,3,4,6,0 };
+    std::deque<int> odd, even;    // odd奇  even偶
+
+    for (auto i : ilist) {
+        // i 和 000000000000001 进行 位与运算，如果i为奇数，则i的最后一位为1，i&1=1
+        ((i & 1) ? odd : even).push_back(i);          // 条件运算符见4.7节
+    }
+
+    std::cout << "odd: ";
+    for (auto o : odd) {
+        std::cout << o << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "even: ";
+    for (auto e : even) {
+        std::cout << e << " ";
+    }
+    std::cout << std::endl;
+}
+
+// 
