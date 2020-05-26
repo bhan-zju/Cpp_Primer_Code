4ccf384a4813431a011b843b4fdbb88211c13c79
锦罽暖亲猫 <55181876+bhan-zju@users.noreply.github.com>
2020-05-26 10:55:23 +0800

    Add files via upload

---
+++ Chapter9/exer9_31.cpp
@@ -0,0 +1,47 @@
+﻿// exer9_31.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <list>
+#include <forward_list>
+
+int main()
+{
+    std::list<int> li{ 9,8,1,7,4,3,0,6,5 };
+    auto iter = li.begin();
+    // 删除偶数元素，复制每个奇数元素
+    while (iter != li.end()) {
+        if (*iter % 2) {
+            iter = li.insert(iter, *iter);
+            // 主要这里不一样
+            ++iter;
+            ++iter;            // iter 向前移动两步
+        }
+        else {
+            iter = li.erase(iter);
+        }
+    }
+    for (auto i : li)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    std::forward_list<int> fi{ 9,8,1,7,4,3,0,6,5 };
+    auto prev = fi.before_begin(), fiter = fi.begin();
+    // 删除偶数元素，复制每个奇数元素
+    while (fiter != fi.end()) {
+        if (*fiter % 2) {
+            prev = fi.insert_after(prev, *fiter);    // insert_after(p, ) 返回指向 最后一个插入元素的 迭代器
+            ++fiter;
+            ++prev;
+        }
+        else {
+            fiter = fi.erase_after(prev);      // erase_after() 返回 指向 被删元素 之后元素的 迭代器
+        }
+    }
+    for (auto i : fi)
+        std::cout << i << " ";
+    std::cout << std::endl;
+}
+
+
+// 
---
+++ Chapter9/prog9_3_6.cpp
@@ -0,0 +1,27 @@
+﻿// prog9_3_6.cpp : 容器操作可能使迭代器失效
+//
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> vi{ 9,8,1,7,4,3,0,6,5 };
+    auto iter = vi.begin();
+    // 删除偶数元素，复制每个奇数元素
+    while (iter != vi.end()) {                 // 不应写为：auto end = vi.end(); while(iter != end);  因为改变vi会导致迭代器end失效，而应该每个循环重新调用一次vi.end()
+        if (*iter % 2) {         // 如果为奇数
+            iter = vi.insert(iter, *iter);          // 复制该奇数，插入到该位置之前，iter指向新插入元素
+            iter += 2;            // iter 向前移动两步
+        }
+        else {        // 如果为偶数
+            iter = vi.erase(iter);       // 删除该元素，iter指向被删元素的下一个元素
+        }
+    }
+
+    for (auto i : vi)
+        std::cout << i << " ";
+    std::cout << std::endl;
+}
+
+// 
