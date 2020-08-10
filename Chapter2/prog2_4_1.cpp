c508143f0623e502b45911407c82a19b70deb825
锦罽暖亲猫 <55181876+bhan-zju@users.noreply.github.com>
2020-08-10 10:56:48 +0800

    Update prog2_4_1.cpp

--- Chapter2/prog2_4_1.cpp
+++ Chapter2/prog2_4_1.cpp
@@ -21,23 +21,13 @@ int main()
         << "r3 = " << r3 << std::endl << std::endl;
 
     double dval = 3.14;
-    const int &ri = dval;
+    const int &ri = dval;                  // ri 是 int 类型的引用
     std::cout << "dval = " << dval << "\n"
-        << "ri = " << ri << std::endl << std::endl;
+        << "ri = " << ri << std::endl << std::endl;      // ri  = 3
     dval = 6.28;
     std::cout << "dval = " << dval << "\n"
-        << "ri = " << ri << std::endl;
+        << "ri = " << ri << std::endl;               // ri 依然为 3
 
     return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
