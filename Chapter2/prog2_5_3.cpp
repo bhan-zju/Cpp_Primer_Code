2e3cff72847a1fa3453429a497f42558805b0f4e
锦罽暖亲猫 <55181876+bhan-zju@users.noreply.github.com>
2020-08-11 09:43:33 +0800

    Update prog2_5_3.cpp

--- Chapter2/prog2_5_3.cpp
+++ Chapter2/prog2_5_3.cpp
@@ -16,8 +16,8 @@ int main()
     decltype(r) b = j;        // int &b 是一个引用，必须初始化
     decltype(r + 0) c;        // r+0的结果是整数，因此是 int c，可以不初始化
     decltype(p) d;            // int *d
-    decltype(*p) e = k;       // 如果表达式是解引用操作，将得到引用类型 int &e，必须初始化
-    decltype((i)) f = m;      // 变量加上一层括号，将会得到引用类型 int &f，必须初始化
+    decltype(*p) e = k;       // 注意！！！！！如果表达式是解引用操作，将得到引用类型 int &e，必须初始化
+    decltype((i)) f = m;      // 注意！！！！！变量加上一层括号，将会得到引用类型 int &f，必须初始化
 
     d = &i;
     *d = 0;
