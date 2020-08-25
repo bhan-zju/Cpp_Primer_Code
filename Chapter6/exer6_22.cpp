faaa0f14038b975e11dac3bc5caded57232809e5
锦罽暖亲猫 <55181876+bhan-zju@users.noreply.github.com>
2020-08-25 16:57:30 +0800

    Update exer6_22.cpp

--- Chapter6/exer6_22.cpp
+++ Chapter6/exer6_22.cpp
@@ -1,9 +1,9 @@
-﻿// exer6_22.cpp : 交换两个int指针
+// exer6_22.cpp : 交换两个int指针
 //
 
 #include <iostream>
 
-void exchangeip(int*& p1, int*& p2)
+void exchangeip(int*& p1, int*& p2)        // p1 p2 是对指针实参的引用
 {
     int* temp = p1;
     p1 = p2;
@@ -14,7 +14,7 @@ int main()
 {
     int i1 = 3, * ip1 = &i1;
     int i2 = 5, * ip2 = &i2;
-    exchangeip(ip1, ip2);
+    exchangeip(ip1, ip2);           // ip1将指向i2，ip2将指向i1
     std::cout << "*ip1 = " << *ip1 << std::endl;
     std::cout << "*ip2 = " << *ip2 << std::endl;
 
